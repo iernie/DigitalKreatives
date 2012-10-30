5386163c64593f3121f5b6d65625c9172c932306
ernie <ernie@epicbytes.net>
2012-10-30 04:23:30 +0100

    bootstrap, new design mock

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,22 @@
-$(document).ready(function(){
-    var slider = $('#slider').bxSlider({
-        pager: false,
-        nextText: '&gt;',
-        prevText: '&lt;',
-        speed: 1500,
-        easing: 'easeOutQuint',
+$(function() {
+    $('.nav li a[href^="#"]').click(function(e) {
+        e.preventDefault();
+        $('html,body').animate({ scrollTop: $(this.hash).offset().top}, 200);
+        return false;
     });
-    
-    $("a.img").fancybox({
-        autoScale: false,
-        overlayOpacity: 0,
+});
+
+$(document).ready(function(){
+    var windowHeight = $(window).height();
+    $(".page").css("min-height", windowHeight-5);
+    $(".work img").each(function() {
+        var imgWidth = $(this).width();
+        $(this).width(imgWidth-20);
+    });
+    $(".page > div").each(function() {
+        var contentHeight = $(this).height()+120;
+        if(contentHeight < windowHeight) {
+            $(this).css('margin-top', (windowHeight/2-contentHeight/2)-100);
+        }
     });
 });
