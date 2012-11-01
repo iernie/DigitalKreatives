7258e24479e1f30f62978179fe7e0b73dd9dcf67
ernie <ernie@epicbytes.net>
2012-11-01 20:40:59 +0100

    revert to when it worked

--- email.php
+++ email.php
@@ -5,11 +5,11 @@
 	$subject = $_POST['subject'];
 	$message = $_POST['message'];
 	$headers =
-		'From: ' . $name . "<". $to .">\r\n" .
+		'From: ' . $name . "<" . $to . ">\r\n" .
 	    'Reply-To: ' . $email . "\r\n" .
 	    'X-Mailer: PHP/' . phpversion();
 
-	if(mail($to, $subject, $message, $headers)) {
+	if (mail($to, $subject, $message, $headers)) {
 		echo "success";
 		exit;
 	} else {
