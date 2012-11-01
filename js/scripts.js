fe60f10e5a48e6fe7300c576135c90dbbf2ffb48
ernie <ernie@epicbytes.net>
2012-11-01 04:43:06 +0100

    form validation, tooltip on arrows and cleaned up js

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,7 @@ $(function() {
 function resizePages() {
     var windowHeight = $(window).height();
     $(".page").css("min-height", windowHeight-35);
-    $(".page > div").each(function() {
+    $(".page").find(".container").each(function() {
         var contentHeight = $(this).height();
         if((contentHeight+120) < windowHeight) {
             $(this).css('margin-top', (windowHeight/2-contentHeight/2)-100);
@@ -17,19 +17,30 @@ function resizePages() {
     });
 }
 
+function isValidEmailAddress(emailAddress) {
+    var pattern = new RegExp(/^(("[\w-+\s]+")|([\w-+]+(?:\.[\w-+]+)*)|("[\w-+\s]+")([\w-+]+(?:\.[\w-+]+)*))(@((?:[\w-+]+\.)*\w[\w-+]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][\d]\.|1[\d]{2}\.|[\d]{1,2}\.))((25[0-5]|2[0-4][\d]|1[\d]{2}|[\d]{1,2})\.){2}(25[0-5]|2[0-4][\d]|1[\d]{2}|[\d]{1,2})\]?$)/i);
+    return pattern.test(emailAddress);
+}
+
 function sendEmail() {
-    var emailName = $("#name").val();
-    var emailEmail = $("#email").val();
-    var emailSubject = $("#subject").val();
-    var emailMessage = $("#message").val();
-    if (emailName === "" || emailEmail === "" || emailSubject === "" || emailMessage === "") {
+    var form = [], formError = false;
+    $("form").find("input,textarea").each(function(index) {
+        form[index] = $(this).val();
+        if (form[index] === "" || ($(this).attr("type") === "email" && !isValidEmailAddress(form[index]))) {
+            $(this).closest(".control-group").addClass("error");
+            formError = true;
+        } else {
+            $(this).closest(".control-group").removeClass("error");
+        }
+    });
+    if (formError) {
         $(".alert-message").html("Please fill all input-fields.");
         return;
     }
     $.ajax({
         type: "POST",
         url: "email.php",
-        data: "name=" + emailName + "&email=" + emailEmail + "&subject=" + emailSubject + "&message=" + emailMessage,
+        data: "name=" + form[0] + "&email=" + form[1] + "&subject=" + form[2] + "&message=" + form[3],
         success: function(msg){
             if (msg === "success") {
                 $(".alert-message").html("Mail sent!");
@@ -43,6 +54,7 @@ function sendEmail() {
 $(document).ready(function(){
     resizePages();
     $(".fancybox").fancybox();
+    $('.arrow-down').find("a").tooltip();
     $("#submit").click(function() {
         sendEmail();
     });
