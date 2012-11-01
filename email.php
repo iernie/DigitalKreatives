26cfaa2465fafa6cccc27e8efee7ae226be221f0
ernie <ernie@epicbytes.net>
2012-11-01 20:09:27 +0100

    more php fixes

--- email.php
+++ email.php
@@ -1,18 +1,13 @@
 <?php
+	$to = "contact@digitalkreatives.net";
 	$name = $_POST['name'];
 	$email = $_POST['email'];
 	$subject = $_POST['subject'];
 	$message = $_POST['message'];
 	$headers =
-		'From: ' . $name . "\r\n" .
+		'From: ' . $name . "<>\r\n" .
 	    'Reply-To: ' . $email . "\r\n" .
 	    'X-Mailer: PHP/' . phpversion();
 
-	if (mail("contact@digitalkreatives.net", $subject, $message, $headers)) {
-		echo "success";
-		exit;
-	} else {
-		echo "error";
-		exit;
-	}
+	return mail($to, $subject, $message, $headers);
 ?>
