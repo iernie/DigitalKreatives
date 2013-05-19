7ed6eb0902106d6be635132a7bc50e1147f87a08
ernie <ernie@epicbytes.net>
2013-05-19 17:18:27 +0200

    changed from form to email

--- js/scripts.js
+++ js/scripts.js
@@ -14,41 +14,6 @@ function resizePages() {
     });
 }
 
-function isValidEmailAddress(emailAddress) {
-    var pattern = new RegExp(/^(("[\w-+\s]+")|([\w-+]+(?:\.[\w-+]+)*)|("[\w-+\s]+")([\w-+]+(?:\.[\w-+]+)*))(@((?:[\w-+]+\.)*\w[\w-+]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][\d]\.|1[\d]{2}\.|[\d]{1,2}\.))((25[0-5]|2[0-4][\d]|1[\d]{2}|[\d]{1,2})\.){2}(25[0-5]|2[0-4][\d]|1[\d]{2}|[\d]{1,2})\]?$)/i);
-    return pattern.test(emailAddress);
-}
-
-function sendEmail() {
-    var form = [], formError = false;
-    $(".alert-message").html("");
-    $("form").find("input,textarea").each(function (index) {
-        form[index] = $(this).val();
-        if (form[index] === "" || ($(this).attr("type") === "email" && !isValidEmailAddress(form[index]))) {
-            $(this).closest(".control-group").addClass("error");
-            formError = true;
-        } else {
-            $(this).closest(".control-group").removeClass("error");
-        }
-    });
-    if (formError) {
-        $(".alert-message").html("Please fill all input-fields.");
-        return;
-    }
-    $.ajax({
-        type: "POST",
-        url: "email.php",
-        data: "name=" + form[0] + "&email=" + form[1] + "&subject=" + form[2] + "&message=" + form[3],
-        success: function (msg) {
-            if (msg === "success") {
-                $(".alert-message").html("Mail sent!");
-            } else {
-                $(".alert-message").html("An error occured while sending the email.");
-            }
-        }
-    });
-}
-
 $(window).load(function () {
     resizePages();
 });
@@ -62,7 +27,4 @@ $(document).ready(function () {
         .fancybox({
             padding: 0
         });
-    $("#submit").click(function () {
-        sendEmail();
-    });
 });
